10060add5b1b19780b78ea5fe6ed138d00de78bf
BlackWhiteYoshi <bastian@firerocket.de>
2023-09-18 15:02:31 +0200

    upgraded to version 1.2.3 updated NuGet packages

--- AutoInterface/Attributes/AttributeInfo.cs
+++ AutoInterface/Attributes/AttributeInfo.cs
@@ -2,5 +2,5 @@
 
 public static partial class Attributes {
     private const string NAME = "AutoInterface";
-    private const string VERSION = "1.2.2";
+    private const string VERSION = "1.2.3";
 }
