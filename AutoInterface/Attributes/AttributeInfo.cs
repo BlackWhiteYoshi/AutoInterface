b917f80bd6b0f5f1e9b35296ad3997fa64bc89a1
BlackWhiteYoshi <bastian@firerocket.de>
2024-04-14 10:53:26 +0200

    upgraded to version 2.3.0

--- AutoInterface/Attributes/AttributeInfo.cs
+++ AutoInterface/Attributes/AttributeInfo.cs
@@ -2,5 +2,5 @@
 
 public static partial class Attributes {
     private const string NAME = "AutoInterface";
-    private const string VERSION = "2.2.1";
+    private const string VERSION = "2.3.0";
 }
